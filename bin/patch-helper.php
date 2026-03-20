29de370de57cec248470a8ec00d08fe5f390488e
Luke Rodgers <luke.rodgers@gene.co.uk>
2026-03-20 09:01:59 +0000

    Increase memory limit

--- bin/patch-helper.php
+++ bin/patch-helper.php
@@ -4,6 +4,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 
+$currentMemoryLimit = ini_get('memory_limit');
+
+if ($currentMemoryLimit !== false && $currentMemoryLimit !== '-1' && (int) $currentMemoryLimit < 4096) {
+    ini_set('memory_limit', '4G');
+}
+
 $application = new Application();
 $analyseCommand = new Ampersand\PatchHelper\Command\AnalyseCommand();
 $application->add($analyseCommand);
